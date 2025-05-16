2a2024679f1cf7ed38845fc2f4e52fec78021fdc
lari88a <laryssavaz888@gmail.com>
2025-05-16 11:29:24 -0300

    Add files via upload

---
+++ Portifolio/script.js
@@ -0,0 +1,5 @@
+// Funcao chamada ao clicar no botao "Enviar"
+function mostrarMensagem() {
+    // Exibe uma mensagem de confirmacao para o usuario
+  alert("Mensagem enviada com sucesso! Obrigado pelo contato.");
+}
